31a5f4f4d23ed500d0ada057a8bc318167f71971
Edleron <Edleron@hotmail.com>
2023-05-21 23:26:04 +0300

    add : level data list wrapper

---
+++ Assets/Scripts/Game/Level/LevelDataListWrapper.cs
@@ -0,0 +1,9 @@
+namespace Game.Level
+{
+    using System.Collections.Generic;
+    [System.Serializable]
+    public class LevelDataListWrapper
+    {
+        public List<LevelData> levelDataList;
+    }
+}
