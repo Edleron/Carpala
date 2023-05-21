b7807b0e1b00d21bc8e71fcda8d51e38b95fe0b0
Edleron <Edleron@hotmail.com>
2023-05-21 14:33:28 +0300

    fix : some changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,31 +16,3 @@ public class GameManager : MonoBehaviour
 
     }
 }
-
-
-public class TestCode : MonoBehaviour
-{
-    [SerializeField]
-    private float _refleshTime = 0.5f;
-
-    private int _frameCount;
-    private float _timeCounter
-    private float _fps;
-
-    private void Update()
-    {
-        if (_timeCounter < _refleshTime)
-        {
-            _timeCounter += Time.deltaTime;
-            _frameCount++;
-        }
-        else
-        {
-            _fps = _frameCount / _timeCounter;
-            _frameCount = 0;
-            _timeCounter = 0;
-        }
-
-        // Debug.Log(_fps);
-    }
-}
