7a30f768120ed4074e2afd1622639035d32e0aa6
Edleron <Edleron@hotmail.com>
2023-06-07 04:20:31 +0300

    fix : zone detect modified

--- Assets/Scripts/Game/Zone/ZoneDetect.cs
+++ Assets/Scripts/Game/Zone/ZoneDetect.cs
@@ -3,36 +3,56 @@ namespace Game.Zone
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using Edleron.Events;
 
     public class ZoneDetect : MonoBehaviour
     {
         public Transform target; // Hedef noktanın referansı
-        private bool checkMove = false;
+        private bool checkDetect = false;
+        private bool checkSwipe = false;
 
         private void Awake()
         {
-            checkMove = false;
+            checkDetect = false;
+            checkSwipe = false;
+        }
+
+        private void OnEnable()
+        {
+            EventManager.onSwipeUp += LaunchControl;
+        }
+
+        private void OnDisable()
+        {
+            EventManager.onSwipeUp -= LaunchControl;
         }
 
         private void Update()
         {
-            if (!checkMove) return;
-            LaunchObject();
+            if (checkDetect && checkSwipe)
+            {
+                LaunchObject();
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag(ZoneTags.Detect))
             {
+                checkDetect = true;
                 Debug.Log(gameObject.name);
-                checkMove = true;
             }
         }
 
-        public void LaunchObject()
+        private void LaunchObject()
         {
             Vector2 currentPosition = transform.position;
             transform.position = Vector2.MoveTowards(currentPosition, target.position, 10.0f * Time.deltaTime);
+        }
+
+        private void LaunchControl()
+        {
+            checkSwipe = true;
         }
     }
 }
