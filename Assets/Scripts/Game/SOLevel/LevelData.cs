14cfc8778e0a778a4d10fb487b9c02f0692bf58f
Edleron <Edleron@hotmail.com>
2023-06-11 23:57:28 +0300

    fix : level system modifieds

--- Assets/Scripts/Game/SOLevel/LevelData.cs
+++ Assets/Scripts/Game/SOLevel/LevelData.cs
@@ -8,7 +8,18 @@ namespace Game.SOLevel
         public string LevelDescription;
         public string LevelDifficulty;
         public int TargetScore;
-        public int[] PrepareStamp;
         public int RotatineSpeed;
+        public int VisibleStamp;
+        public int[] PrepareStamp;
+        public int[] PrepareValue;
+        public ResultData[] PrepareResult;
+    }
+
+    [System.Serializable]
+    public class ResultData
+    {
+        public int result;
+        public int valueOne;
+        public int valueTwo;
     }
 }
--- Assets/Scripts/Game/SOLevel/LevelManager.cs
+++ Assets/Scripts/Game/SOLevel/LevelManager.cs
@@ -11,26 +11,71 @@ namespace Game.SOLevel
 
         public List<LevelDataScriptableObject> levelList;
 
+        private int LevelIndex { get; set; }
+
         private void Awake()
         {
             Instance = this;
-            Debug.Log(GetLevelName());
+            LevelIndex = 0; // Todo PlayerPrefs
         }
 
-        public string GetLevelName()
+        #region Level Process
+        public void SetLevel()
         {
-            return levelList[0].levelData.LevelName;
+            LevelIndex++;
         }
 
+        public LevelData GetLevel()
+        {
+            return levelList[LevelIndex].levelData;
+        }
+        #endregion
+
+
+        #region Level Prepare
         public int GetRotationSpeed()
         {
-            return levelList[0].levelData.RotatineSpeed;
+            return levelList[LevelIndex].levelData.RotatineSpeed;
         }
 
         public int[] GetPrepareStamp()
         {
-            return levelList[0].levelData.PrepareStamp;
+            return levelList[LevelIndex].levelData.PrepareStamp;
         }
 
+        public int[] GetPrepareValue()
+        {
+            int[] values = levelList[LevelIndex].levelData.PrepareValue;
+            return ShuffleArray(values);
+        }
+
+        public int[] GetPrepareResult()
+        {
+            int length = levelList[LevelIndex].levelData.PrepareResult.Length;
+            int indis = Random.Range(1, length);
+
+            int[] arr = new int[2];
+            arr[0] = levelList[LevelIndex].levelData.PrepareResult[indis].valueOne;
+            arr[1] = levelList[LevelIndex].levelData.PrepareResult[indis].valueTwo;
+            return arr;
+        }
+        #endregion
+
+        #region HELPERS
+        public int[] ShuffleArray(int[] array)
+        {
+            System.Random random = new System.Random();
+
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+
+            return array;
+        }
+        #endregion
     }
 }
