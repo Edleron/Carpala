668248be1e21f58933de283f9523074ad1b4150c
Edleron <Edleron@hotmail.com>
2023-06-22 12:54:04 +0300

    fix : pull system modified

--- Assets/Scripts/Game/Pull/PullController.cs
+++ Assets/Scripts/Game/Pull/PullController.cs
@@ -21,13 +21,11 @@ namespace Game.Pull
 
         private void OnEnable()
         {
-            EventManager.onPull += pullVisualizer.PullAnimActiveTrue;
             EventManager.onSwipeDown += pullVisualizer.PullAnimPassiveTrue;
         }
 
         private void OnDisable()
         {
-            EventManager.onPull -= pullVisualizer.PullAnimActiveTrue;
             EventManager.onSwipeDown -= pullVisualizer.PullAnimPassiveTrue;
         }
 
