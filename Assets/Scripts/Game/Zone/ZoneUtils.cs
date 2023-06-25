4bc089758bf9c5a5cf04e5fbd1aa8fc40ede4cac
Edleron <Edleron@hotmail.com>
2023-06-25 14:44:48 +0300

    fix : deleting some changes

--- Assets/Scripts/Game/Zone/ZoneUtils.cs
+++
@@ -1,7 +0,0 @@
-public static class ZoneUtils
-{
-    public const string Detect = "Detect";
-    public const string Target = "Target";
-    public const string Zone = "Zone";
-    public static bool IsInCheckSwipe { get; set; }
-}
