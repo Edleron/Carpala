bd760eadfb85d885829ac058befb0e27571ef4f2
Edleron <Edleron@hotmail.com>
2023-06-25 14:45:23 +0300

    fix : contants new script name

---
+++ Assets/Scripts/Game/Constants/Constants.cs
@@ -0,0 +1,7 @@
+public static class Constants
+{
+    public const string Detect = "Detect";
+    public const string Target = "Target";
+    public const string Zone = "Zone";
+    public static bool IsInCheckSwipe { get; set; }
+}
