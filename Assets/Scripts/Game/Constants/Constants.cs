8aa94e4d3121a837944596a756fcc9f160e30bda
Edleron <Edleron@hotmail.com>
2023-07-10 11:07:18 +0300

    fix : constants

--- Assets/Scripts/Game/Constants/Constants.cs
+++ Assets/Scripts/Game/Constants/Constants.cs
@@ -3,5 +3,4 @@ public static class Constants
     public const string Detect = "Detect";
     public const string Target = "Target";
     public const string Zone = "Zone";
-    public static bool IsInCheckSwipe { get; set; }
 }
