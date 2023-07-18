1308ecd916af8b2daae86c8c98401fa31f513f06
Edleron <Edleron@hotmail.com>
2023-07-18 18:56:18 +0300

    add : UI script addings

---
+++ Assets/Scripts/Game/UI/UIVisualizer.cs
@@ -0,0 +1,11 @@
+namespace Game.UI
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class UIVisualizer : MonoBehaviour
+    {
+
+    }
+}
