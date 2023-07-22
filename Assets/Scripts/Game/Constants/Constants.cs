3b396d15668a095ea027a5022fdf192a14e214c1
Edleron <Edleron@hotmail.com>
2023-07-22 14:54:22 +0300

    add : ui button name constants

--- Assets/Scripts/Game/Constants/Constants.cs
+++ Assets/Scripts/Game/Constants/Constants.cs
@@ -1,6 +1,26 @@
 public static class Constants
 {
+    #region Game
     public const string Detect = "Detect";
     public const string Target = "Target";
     public const string Zone = "Zone";
+    #endregion
+
+    #region ButtonUIName
+    // GAME
+    public const string Settings = "Settings";
+    public const string Clue = "Clue";
+
+    // TOP
+    public const string Closed = "Closed";
+    public const string Achievements = "Achievements";
+    public const string Sound = "Sound";
+    public const string Back = "Back";
+
+    // MENU
+    public const string Info = "Info";
+    public const string Rhythmic = "Rhythmic";
+    public const string Played = "Played";
+    public const string Preferences = "Preferences";
+    #endregion
 }
