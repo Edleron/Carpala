0e2895afcaf19e43ba40643b98159304313b681a
Edleron <Edleron@hotmail.com>
2023-07-24 14:26:55 +0300

    fix : game scene rhythmic feature

--- Assets/Scripts/Game/Rythic/RhytmicManager.cs
+++ Assets/Scripts/Game/Rythic/RhytmicManager.cs
@@ -8,17 +8,8 @@ namespace Game.Rhythmic
     public class RhythmicManager : MonoBehaviour
     {
         public static RhythmicManager Instance { get; private set; }
-        public List<List<GameObject>> dummyList = new List<List<GameObject>>();
         public List<List<string>> checkList = new List<List<string>>();
-        public List<GameObject> rhythmicResult_0 = new List<GameObject>();
-        public List<GameObject> rhythmicResult_1 = new List<GameObject>();
-        public List<GameObject> rhythmicResult_2 = new List<GameObject>();
-        public List<GameObject> rhythmicResult_3 = new List<GameObject>();
-        public List<GameObject> rhythmicResult_4 = new List<GameObject>();
-        public List<GameObject> rhythmicResult_5 = new List<GameObject>();
-        public List<GameObject> rhythmicResult_6 = new List<GameObject>();
-        public List<GameObject> rhythmicResult_7 = new List<GameObject>();
-        public List<GameObject> rhythmicResult_8 = new List<GameObject>();
+        public List<RhtyhmicDataAO> rhytmicData = new List<RhtyhmicDataAO>();
 
         private void Awake()
         {
@@ -28,18 +19,8 @@ namespace Game.Rhythmic
 
         public void SetDict()
         {
-            dummyList.Add(rhythmicResult_0);
-            dummyList.Add(rhythmicResult_1);
-            dummyList.Add(rhythmicResult_2);
-            dummyList.Add(rhythmicResult_3);
-            dummyList.Add(rhythmicResult_4);
-            dummyList.Add(rhythmicResult_5);
-            dummyList.Add(rhythmicResult_6);
-            dummyList.Add(rhythmicResult_7);
-            dummyList.Add(rhythmicResult_8);
-
             // Todo
-            for (int i = 0; i < dummyList.Count; i++)
+            for (int i = 0; i < rhytmicData.Count; i++)
             {
                 checkList.Add(new List<string>()); // Todo
             }
@@ -47,7 +28,8 @@ namespace Game.Rhythmic
 
         public void SetRhytmic(int level, string result)
         {
-            List<GameObject> innerList = dummyList[level];
+            /*
+            List<GameObject> innerList = rhytmicData[level].list;
 
             string fileName = "Detect - " + result;
 
@@ -59,6 +41,7 @@ namespace Game.Rhythmic
                     checkList[level].Add(obj.name);
                 }
             }
+            */
         }
 
         public void SaveResultListToJson(string filePath)
