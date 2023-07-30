ba2a8fbefb4e7b70bcca5ad73b1e5af06039a512
Edleron <Edleron@hotmail.com>
2023-07-30 04:48:13 +0300

    fix : player prefs

--- Assets/Scripts/Game/PlayerPrefs/PlayerPrefsManager.cs
+++ Assets/Scripts/Game/PlayerPrefs/PlayerPrefsManager.cs
@@ -2,30 +2,37 @@ namespace Game.PlayerPrefs
 {
     using UnityEngine;
 
-    [CreateAssetMenu(fileName = "PlayerPrefs", menuName = "Custom/PlayerPrefs")]
-    public class PlayerPrefsManager : ScriptableObject
+    public class PlayerPrefsManager : MonoBehaviour
     {
-        public int Level;
-        public int Heart;
+        public static PlayerPrefsManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+        }
 
         public void SaveLevel(int value)
         {
-            Level = value;
+            PlayerPrefs.SetInt("Level", value);
+            PlayerPrefs.Save();
         }
 
         public int LoadLevel()
         {
-            return Level;
+            int value = PlayerPrefs.HasKey("Level") == true ? PlayerPrefs.GetInt("Level") : 0;
+            return value;
         }
 
         public void SaveHeart(int value)
         {
-            Heart = value;
+            PlayerPrefs.SetInt("Heart", value);
+            PlayerPrefs.Save();
         }
 
         public int LoadHeart()
         {
-            return Heart;
+            int value = PlayerPrefs.HasKey("Heart") == true ? PlayerPrefs.GetInt("Heart") : 0;
+            return value;
         }
     }
 }
