44e27906e7e7c6f81997e408bbc6885d8420ba9e
Edleron <Edleron@hotmail.com>
2023-08-03 13:29:49 +0300

    fix : uı script modified

--- Assets/Scripts/Game/UI/UIVisualizer.cs
+++ Assets/Scripts/Game/UI/UIVisualizer.cs
@@ -3,9 +3,7 @@ namespace Game.UI
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
-    using Edleron.Events;
-    using Game.SOLevel;
-    using Game.PlayerPrefs;
+    using Game.Road;
 
     public class UIVisualizer : MonoBehaviour
     {
@@ -17,6 +15,11 @@ namespace Game.UI
         public GameObject SliderPanel;
         public GameObject TutorialsPanel;
         public List<GameObject> TutorialState;
+
+        public GameObject SoundObje;
+        private bool SoundState = false;
+        public List<Sprite> sTexture = new List<Sprite>();
+
         private int TutorialCount = 0;
         private bool locked { get; set; }
 
@@ -48,13 +51,33 @@ namespace Game.UI
             ButtonList[2].name = Constants.Closed;
             ButtonList[3].name = Constants.Restart;
             ButtonList[4].name = Constants.Sound;
-            ButtonList[5].name = Constants.Back;
+            ButtonList[5].name = Constants.Achievements;
+            ButtonList[6].name = Constants.Result;
+            ButtonList[7].name = Constants.Back;
 
             // TOP Menu
-            ButtonList[6].name = Constants.Info;
-            ButtonList[7].name = Constants.Rhythmic;
-            ButtonList[8].name = Constants.Played;
-            ButtonList[9].name = Constants.Preferences;
+            ButtonList[8].name = Constants.Info;
+            ButtonList[9].name = Constants.Rhythmic;
+            ButtonList[10].name = Constants.Played;
+            ButtonList[11].name = Constants.Preferences;
+        }
+
+        public void onClick_EndGame()
+        {
+            if (!locked)
+            {
+                return;
+            }
+
+            ButtonList[2].gameObject.SetActive(false);
+
+            UIMenuPanel.SetActive(true);
+            SliderPanel.SetActive(false);
+            GameMenuPanel.SetActive(false);
+            TutorialsPanel.SetActive(false);
+
+            OpenMenuPanel(5);
+            RoadVisualizer.Instance.SetRoad();
         }
 
         public void onClick_Setting()
@@ -63,6 +86,8 @@ namespace Game.UI
             {
                 return;
             }
+
+            ButtonList[2].gameObject.SetActive(true);
 
             UIMenuPanel.SetActive(true);
             SliderPanel.SetActive(false);
@@ -146,6 +171,44 @@ namespace Game.UI
         public void onClick_Sound()
         {
             // TODO
+            SpriteRenderer renderer = SoundObje.GetComponent<SpriteRenderer>();
+            AudioSource audioListener = GetComponent<AudioSource>();
+            SoundState = !SoundState;
+            if (SoundState)
+            {
+                if (renderer != null && sTexture[1] != null)
+                {
+                    // Texture'ı değiştirme
+                    renderer.sprite = sTexture[1];
+                    audioListener.enabled = false;
+                }
+                else
+                {
+                    Debug.Log("Hata");
+                }
+            }
+            else
+            {
+                if (renderer != null && sTexture[0] != null)
+                {
+                    // Texture'ı değiştirme
+                    renderer.sprite = sTexture[0];
+                    audioListener.enabled = true;
+                }
+                else
+                {
+                    Debug.Log("Hata");
+                }
+            }
+        }
+        public void onClick_Achievements()
+        {
+            OpenMenuPanel(5);
+            RoadVisualizer.Instance.SetRoad();
+        }
+        public void onClick_Result()
+        {
+            OpenMenuPanel(2);
         }
 
         public void onClick_Back()
@@ -175,6 +238,15 @@ namespace Game.UI
 
         private void OpenMenuPanel(int value)
         {
+            if (value == 0)
+            {
+                ButtonList[7].gameObject.SetActive(false);
+            }
+            else
+            {
+                ButtonList[7].gameObject.SetActive(true);
+            }
+
             for (int i = 0; i < MenuPanelList.Count; i++)
             {
                 MenuPanelList[i].SetActive(false);
