ae1bb8b452339c23501697f3fdb66c0ce821a393
Edleron <Edleron@hotmail.com>
2023-08-18 12:32:48 +0300

    fix : popup new feature

--- Assets/Scripts/Game/Popup/PopupVisualizer.cs
+++ Assets/Scripts/Game/Popup/PopupVisualizer.cs
@@ -10,13 +10,16 @@ namespace Game.Popup
         public static PopupVisualizer Instance { get; private set; }
 
         public GameObject Popup;
-        public GameObject Button;
+        public GameObject CloseButton;
+        public GameObject OkButton;
         public TextMeshPro CorrectText;
         public TextMeshPro WrongText;
+        public GameObject Oriantation;
 
         private void Awake()
         {
             Instance = this;
+            Oriantation.SetActive(false);
         }
 
         private void Start()
@@ -29,51 +32,64 @@ namespace Game.Popup
 
         public void ActivePopup(int correct, int wrong)
         {
-            Button.SetActive(true);
+            CloseButton.SetActive(true);
+            OkButton.SetActive(true);
             Popup.SetActive(true);
             CorrectText.text = "Doğru Sayısı : " + correct.ToString();
             WrongText.text = "Yanlış Sayısı : " + wrong.ToString();
+            Oriantation.SetActive(false);
         }
 
         public void PassivePopup()
         {
-            Button.SetActive(true);
+            CloseButton.SetActive(true);
+            OkButton.SetActive(true);
             Popup.SetActive(false);
             CorrectText.text = " ";
             WrongText.text = " ";
+            Oriantation.SetActive(false);
         }
 
         public void ActiveHeart()
         {
-            Button.SetActive(true);
+            CloseButton.SetActive(true);
+            OkButton.SetActive(true);
             Popup.SetActive(true);
             CorrectText.text = "Canınız Yetersizdir, Bir önceki Seviyeyi Tekrar Etmek Zorundasınız.";
             WrongText.text = " ";
+            Oriantation.SetActive(false);
         }
 
 
         public void ActiveTimer()
         {
-            Button.SetActive(true);
+            CloseButton.SetActive(true);
+            OkButton.SetActive(true);
             Popup.SetActive(true);
             CorrectText.text = "Süreniz Bitmiştir, Bir önceki Seviyeyi Tekrar Etmek Zorundasınız.";
             WrongText.text = " ";
+            Oriantation.SetActive(false);
         }
 
         public void ActiveRestart()
         {
-            Button.SetActive(true);
+            CloseButton.SetActive(true);
+            OkButton.SetActive(true);
             Popup.SetActive(true);
             CorrectText.text = "Oyun Yeniden Sıfır Ayarlarında Başlatılacaktır.";
             WrongText.text = " ";
+            Oriantation.SetActive(false);
         }
 
         public void EndGame()
         {
-            Button.SetActive(false);
+            CloseButton.SetActive(false);
+            OkButton.SetActive(false);
             Popup.SetActive(true);
             CorrectText.text = "Oyun Başarıyla Tamamlanmıştır.";
             WrongText.text = " ";
+
+            Oriantation.SetActive(true);
         }
     }
 }
